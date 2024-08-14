6cb34f39f45733639d4834561496bf0a48a5559f
srath.dataengineer@gmail.com <srath.dataengineer@gmail.com>
2024-08-14 20:00:38 +0000

    1st commit

---
+++ Module1/Understanding Delta Lake.sql
@@ -0,0 +1,37 @@
+-- Databricks notebook source
+CREATE TABLE employees(id INT, name STRING, salary DOUBLE);
+
+-- COMMAND ----------
+
+drop table default.employees;
+
+-- COMMAND ----------
+
+select * from default.employees;
+
+-- COMMAND ----------
+
+insert into employees values 
+(1,'Adam',2500.0),
+(2,'Sarah',3000.5),
+(3, "John", 2999.3),
+(4, "Thomas", 4000.3),
+(5, "Anna", 2500.0),
+(6, "Kim", 6200.3);
+
+
+-- COMMAND ----------
+
+update employees set salary = salary +100 where name like 'A%';
+
+-- COMMAND ----------
+
+describe detail employees;
+
+-- COMMAND ----------
+
+describe history employees;
+
+-- COMMAND ----------
+
+-- MAGIC %fs ls 'dbfs:/user/hive/warehouse/employees'
